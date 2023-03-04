8341566b51a91c51bfbbf982fa28cbb20eb5a6a7
junhui-LEE <junhee0386@naver.com>
2023-03-04 18:33:39 +0900

    1) 게시판 CRUD 기능 구현
    버튼을 클릭했을때 발생하는 이벤트는 jQuery로 구현했고 네이버카페를 모티브로 해서 기본적인 게시물 등록, 수정, 읽기, 삭제를 처리 했다. 이때 게시물을 읽은 다음에 "목록"버튼을 누름으로서 사용자가 들어왔던 페이지로 이동이 가능하게 처리 했다. 하지만 사용자가 게시물에서 "수정"이나 "삭제" 버튼을 눌렀을때에는 들어왔던 페이지가 아니라 1page로 이동하는 이슈가 있었다. 이를 해결하기 위해서 많은 시간동안 디버깅을 해 봤는데 modify()나 remove()를 통과하여 정상적으로 list()메서드의 인자로page 와 pageSize가 넘어오지만 if(page==null) page=1에 걸리는 것을 확인했다. 이를 어떻게 해결해야 할지 모르겠다. => 발생한 이슈이다.

--- src/main/java/org/example/controller/BoardController.java
+++ src/main/java/org/example/controller/BoardController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -18,14 +21,95 @@ import java.util.Map;
 public class BoardController {
     @Autowired
     BoardService boardService;
+    @PostMapping("/modify")
+    public String modify(BoardDto boardDto, Integer page, Integer pageSize, RedirectAttributes rattr, Model m, HttpSession session) {
+
+        String writer = (String)session.getAttribute("id");
+        boardDto.setWriter(writer);
+
+        try {
+            if (boardService.modify(boardDto)!= 1)
+                throw new Exception("Modify failed.");
+
+            rattr.addAttribute("page", page);
+            rattr.addAttribute("pageSize", pageSize);
+            rattr.addFlashAttribute("msg", "MOD_OK");
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+            m.addAttribute("msg", "MOD_ERR");
+            return "board"; // 등록하려던 내용을 보여줘야 함.
+        }
+    }
+
+
+    @PostMapping("/write") // insert니까 delete인 remove하고 동일
+    public String write(BoardDto boardDto, RedirectAttributes rattr, Model m, HttpSession session) {
+        String writer = (String)session.getAttribute("id");
+        boardDto.setWriter(writer);
+        try {
+            if (boardService.write(boardDto) != 1)
+                throw new Exception("Write failed.");
+            rattr.addFlashAttribute("msg", "WRT_OK");
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute("mode", "new"); // 글쓰기 모드로
+            m.addAttribute(boardDto);      // 등록하려던 내용을 보여줘야 함.
+            m.addAttribute("msg", "WRT_ERR");
+            return "board";
+        }
+    }
+
+    @GetMapping("/write")
+    public String write(Model m){
+        m.addAttribute("mode", "new");
+        return "board";
+    }
+
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, RedirectAttributes rattr, Model m, HttpSession session){
+       String writer = (String)session.getAttribute("id");
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+           int rowCnt = boardService.remove(bno, writer);
+            if(rowCnt != 1)
+                throw new Exception("board remove error");
+            rattr.addFlashAttribute("msg", "DEL_OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+            rattr.addFlashAttribute("msg", "DEL_ERR");
+        }
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize, Model m){//1234
+        try {
+            BoardDto boardDto = boardService.read(bno);
+//        m.addAttribute("boardDto", boardDto);  // 아래 문장과 동일
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "board";
+    }
+
+
     @GetMapping("/list")
     // 게시물리스트를 보여주는 메서드
-    public String list(Integer page, Integer pageSize, Model m, HttpServletRequest request) {
+    public String list(@RequestParam(defaultValue ="1") Integer page, @RequestParam(defaultValue = "10") Integer pageSize, Model m, HttpServletRequest request) {
         if(!loginCheck(request))
             return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
 
-        if(page == null) page=1;
-        if(pageSize == null) pageSize=10;
+//        if(page == null) page=1;
+//        if(pageSize == null) pageSize=10;
 
         try {
             int totalCnt = boardService.getCount();
@@ -38,6 +122,8 @@ public class BoardController {
            List<BoardDto> list = boardService.getPage(map);
            m.addAttribute("list", list);
            m.addAttribute("ph", pageHandler);
+           m.addAttribute("page", page);
+           m.addAttribute("pageSize", pageSize);
         } catch (Exception e) {
            e.printStackTrace();
         }
